6c3b9d2f1cc34317a2a77c1bdbaaed13b5c6d621
Weston Sandfort <sandfortw@gmail.com>
2023-01-01 13:33:40 -0700

    Add JungleBeat class

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,6 +1,19 @@
 class JungleBeat
+  attr_reader :list
 
   def initialize
     @list = LinkedList.new
   end
+
+  def append(string)
+    string.scan(/\w+/).each do |element|
+      node = Node.new(element)
+      @list.append(node)
+    end
+  end
+
+  def count
+    @list.count
+  end
+
 end
